78da0fed590448a02444aa8532d4d27956618aca
root <root@example.com>
2024-10-28 18:26:02 +0800

    save progress

---
+++ pkt_7_base.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: pkt_7_base
+# Author: Barry Duggan
+# Description: Baseband packet
+# GNU Radio version: 3.10.9.2
+
+from gnuradio import blocks
+import pmt
+from gnuradio import blocks, gr
+from gnuradio import digital
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import gr, pdu
+
+
+
+
+class pkt_7_base(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "pkt_7_base", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.num_key = num_key = "packet_num"
+        self.len_key = len_key = "packet_len"
+        self.samp_rate = samp_rate = 32e3
+        self.hdr_format = hdr_format = digital.header_format_crc(len_key, num_key)
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.pdu_tagged_stream_to_pdu_0 = pdu.tagged_stream_to_pdu(gr.types.byte_t, 'packet_len')
+        self.pdu_pdu_to_tagged_stream_0_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
+        self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
+        self.digital_protocol_parser_b_0 = digital.protocol_parser_b(hdr_format)
+        self.digital_protocol_formatter_async_0 = digital.protocol_formatter_async(hdr_format)
+        self.digital_header_payload_demux_0 = digital.header_payload_demux(
+            32,
+            1,
+            0,
+            "packet_len",
+            "packet_len",
+            False,
+            gr.sizeof_char,
+            "burst",
+            int(samp_rate),
+            (),
+            0)
+        self.digital_crc32_async_bb_2 = digital.crc32_async_bb(False)
+        self.digital_crc32_async_bb_0 = digital.crc32_async_bb(True)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
+        self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, '', "")
+        self.blocks_tag_debug_0.set_display(True)
+        self.blocks_repack_bits_bb_1_0 = blocks.repack_bits_bb(8, 1, "packet_len", False, gr.GR_MSB_FIRST)
+        self.blocks_repack_bits_bb_1 = blocks.repack_bits_bb(1, 8, "packet_len", False, gr.GR_MSB_FIRST)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.cons(pmt.PMT_NIL,pmt.init_u8vector(9,(71,78,85,32,82,97,100,105,111))), 2000)
+        self.blocks_message_debug_1 = blocks.message_debug(True, gr.log_levels.info)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.digital_crc32_async_bb_2, 'in'))
+        self.msg_connect((self.digital_crc32_async_bb_0, 'out'), (self.blocks_message_debug_1, 'print'))
+        self.msg_connect((self.digital_crc32_async_bb_2, 'out'), (self.digital_protocol_formatter_async_0, 'in'))
+        self.msg_connect((self.digital_protocol_formatter_async_0, 'header'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
+        self.msg_connect((self.digital_protocol_formatter_async_0, 'payload'), (self.pdu_pdu_to_tagged_stream_0_0, 'pdus'))
+        self.msg_connect((self.digital_protocol_parser_b_0, 'info'), (self.digital_header_payload_demux_0, 'header_data'))
+        self.msg_connect((self.pdu_tagged_stream_to_pdu_0, 'pdus'), (self.digital_crc32_async_bb_0, 'in'))
+        self.connect((self.blocks_repack_bits_bb_1, 0), (self.blocks_tag_debug_0, 0))
+        self.connect((self.blocks_repack_bits_bb_1, 0), (self.pdu_tagged_stream_to_pdu_0, 0))
+        self.connect((self.blocks_repack_bits_bb_1_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_repack_bits_bb_1_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.digital_header_payload_demux_0, 0))
+        self.connect((self.digital_header_payload_demux_0, 1), (self.blocks_repack_bits_bb_1, 0))
+        self.connect((self.digital_header_payload_demux_0, 0), (self.digital_protocol_parser_b_0, 0))
+        self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+        self.connect((self.pdu_pdu_to_tagged_stream_0_0, 0), (self.blocks_tagged_stream_mux_0, 1))
+
+
+    def get_num_key(self):
+        return self.num_key
+
+    def set_num_key(self, num_key):
+        self.num_key = num_key
+        self.set_hdr_format(digital.header_format_crc(self.len_key, self.num_key))
+
+    def get_len_key(self):
+        return self.len_key
+
+    def set_len_key(self, len_key):
+        self.len_key = len_key
+        self.set_hdr_format(digital.header_format_crc(self.len_key, self.num_key))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+    def get_hdr_format(self):
+        return self.hdr_format
+
+    def set_hdr_format(self, hdr_format):
+        self.hdr_format = hdr_format
+
+
+
+
+def main(top_block_cls=pkt_7_base, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
--- test_packet_mux_and_demux.py
+++ test_packet_mux_and_demux.py
@@ -5,13 +5,13 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Send and receive packetized stream using demuxer
+# Title: Test packet mux and demux
 # GNU Radio version: 3.10.9.2
 
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio import blocks
-import numpy
+import pmt
 from gnuradio import blocks, gr
 from gnuradio import digital
 from gnuradio import gr
@@ -24,16 +24,18 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import gr, pdu
+from gnuradio import pdu
+import pmt, numpy as np
 import sip
 
 
 
-class packetize2(gr.top_block, Qt.QWidget):
+class test_packet_mux_and_demux(gr.top_block, Qt.QWidget):
 
-    def __init__(self):
-        gr.top_block.__init__(self, "Send and receive packetized stream using demuxer", catch_exceptions=True)
+    def __init__(self, out='out.bin'):
+        gr.top_block.__init__(self, "Test packet mux and demux", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Send and receive packetized stream using demuxer")
+        self.setWindowTitle("Test packet mux and demux")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -51,7 +53,7 @@ class packetize2(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "packetize2")
+        self.settings = Qt.QSettings("GNU Radio", "test_packet_mux_and_demux")
 
         try:
             geometry = self.settings.value("geometry")
@@ -61,18 +63,23 @@ class packetize2(gr.top_block, Qt.QWidget):
             print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
+        # Parameters
+        ##################################################
+        self.out = out
+
+        ##################################################
         # Variables
         ##################################################
         self.access_code = access_code = "11100001010110101110100010010011"
-        self.samp_rate = samp_rate = 48000
-        self.format_obj = format_obj = digital.header_format_default(access_code, 0)
+        self.samp_rate = samp_rate = 32000
+        self.format_used = format_used = digital.header_format_counter(access_code,0,8)
 
         ##################################################
         # Blocks
         ##################################################
 
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            1024, #size
+            21, #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
@@ -119,96 +126,107 @@ class packetize2(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.pdu_tagged_stream_to_pdu_0 = pdu.tagged_stream_to_pdu(gr.types.byte_t, 'packet_len')
-        self.digital_protocol_parser_b_0 = digital.protocol_parser_b(format_obj)
-        self.digital_protocol_formatter_bb_0 = digital.protocol_formatter_bb(format_obj, "packet_len")
+        self.pdu_pdu_to_tagged_stream_0_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
+        self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
+        self.pdu_pdu_lambda_0 = pdu.pdu_lambda(lambda x: pmt.dict_add(x, pmt.string_to_symbol("frame_len"), pmt.from_long(8*pmt.to_long(pmt.dict_ref(x,pmt.string_to_symbol("payload symbols"),pmt.from_long(0))))), "RAW", pmt.intern("key"))
+        self.digital_protocol_parser_b_0 = digital.protocol_parser_b(format_used)
+        self.digital_protocol_formatter_async_0 = digital.protocol_formatter_async(format_used)
         self.digital_header_payload_demux_0 = digital.header_payload_demux(
-            64,
+            96,
             1,
             0,
-            "payload symbols",
-            '',
+            "frame_len",
+            "packet_len",
             False,
             gr.sizeof_char,
-            '',
-            int(samp_rate),
+            "",
+            samp_rate,
             (),
             0)
-        self.digital_crc32_bb_1 = digital.crc32_bb(True, "packet_len", True)
-        self.digital_crc32_bb_0 = digital.crc32_bb(False, "packet_len", True)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, "packet_len", 0)
-        self.blocks_tag_gate_0 = blocks.tag_gate(gr.sizeof_char * 1, False)
-        self.blocks_tag_gate_0.set_single_key("")
-        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 64, "packet_len")
-        self.blocks_repack_bits_bb_1 = blocks.repack_bits_bb(1, 8, "", False, gr.GR_LSB_FIRST)
-        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, 8, "packet_len", False, gr.GR_MSB_FIRST)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_char*1)
-        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
+        self.digital_crc32_async_bb_0 = digital.crc32_async_bb(False)
+        self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
+        self.blocks_repack_bits_bb_1 = blocks.repack_bits_bb(1, 8, "frame_len", False, gr.GR_MSB_FIRST)
+        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(8, 1, "packet_len", False, gr.GR_MSB_FIRST)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.cons(pmt.PMT_NIL, pmt.init_u8vector(6,[0x01,0x02,0x03,0x04,0x00,0x00])), 1000)
+        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.debug)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, out, False)
+        self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
-        self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 256, 1000))), True)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.digital_protocol_parser_b_0, 'info'), (self.digital_header_payload_demux_0, 'header_data'))
-        self.msg_connect((self.pdu_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_0, 'print'))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.digital_crc32_async_bb_0, 'in'))
+        self.msg_connect((self.digital_crc32_async_bb_0, 'out'), (self.digital_protocol_formatter_async_0, 'in'))
+        self.msg_connect((self.digital_protocol_formatter_async_0, 'header'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
+        self.msg_connect((self.digital_protocol_formatter_async_0, 'payload'), (self.pdu_pdu_to_tagged_stream_0_0, 'pdus'))
+        self.msg_connect((self.digital_protocol_parser_b_0, 'info'), (self.pdu_pdu_lambda_0, 'pdu'))
+        self.msg_connect((self.pdu_pdu_lambda_0, 'pdu'), (self.blocks_message_debug_0, 'print'))
+        self.msg_connect((self.pdu_pdu_lambda_0, 'pdu'), (self.digital_header_payload_demux_0, 'header_data'))
         self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_repack_bits_bb_0, 0), (self.digital_crc32_bb_1, 0))
-        self.connect((self.blocks_repack_bits_bb_1, 0), (self.digital_header_payload_demux_0, 0))
-        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_crc32_bb_0, 0))
-        self.connect((self.blocks_tag_gate_0, 0), (self.blocks_repack_bits_bb_1, 0))
-        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_tag_gate_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
-        self.connect((self.digital_crc32_bb_0, 0), (self.blocks_tagged_stream_mux_0, 1))
-        self.connect((self.digital_crc32_bb_0, 0), (self.digital_protocol_formatter_bb_0, 0))
-        self.connect((self.digital_crc32_bb_1, 0), (self.pdu_tagged_stream_to_pdu_0, 0))
-        self.connect((self.digital_header_payload_demux_0, 1), (self.blocks_char_to_float_0, 0))
-        self.connect((self.digital_header_payload_demux_0, 1), (self.blocks_null_sink_0, 0))
-        self.connect((self.digital_header_payload_demux_0, 1), (self.blocks_repack_bits_bb_0, 0))
+        self.connect((self.blocks_repack_bits_bb_0, 0), (self.digital_header_payload_demux_0, 0))
+        self.connect((self.blocks_repack_bits_bb_1, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_repack_bits_bb_0, 0))
+        self.connect((self.digital_header_payload_demux_0, 1), (self.blocks_repack_bits_bb_1, 0))
         self.connect((self.digital_header_payload_demux_0, 0), (self.digital_protocol_parser_b_0, 0))
-        self.connect((self.digital_protocol_formatter_bb_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+        self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+        self.connect((self.pdu_pdu_to_tagged_stream_0_0, 0), (self.blocks_tagged_stream_mux_0, 1))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "packetize2")
+        self.settings = Qt.QSettings("GNU Radio", "test_packet_mux_and_demux")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
+    def get_out(self):
+        return self.out
+
+    def set_out(self, out):
+        self.out = out
+        self.blocks_file_sink_0.open(self.out)
+
     def get_access_code(self):
         return self.access_code
 
     def set_access_code(self, access_code):
         self.access_code = access_code
-        self.set_format_obj(digital.header_format_default(self.access_code, 0))
+        self.set_format_used(digital.header_format_counter(self.access_code,0,8))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_format_obj(self):
-        return self.format_obj
+    def get_format_used(self):
+        return self.format_used
 
-    def set_format_obj(self, format_obj):
-        self.format_obj = format_obj
-
-
+    def set_format_used(self, format_used):
+        self.format_used = format_used
 
 
-def main(top_block_cls=packetize2, options=None):
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--out", dest="out", type=str, default='out.bin',
+        help="Set Output filename [default=%(default)r]")
+    return parser
+
+
+def main(top_block_cls=test_packet_mux_and_demux, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
 
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls()
+    tb = top_block_cls(out=options.out)
 
     tb.start()
 
