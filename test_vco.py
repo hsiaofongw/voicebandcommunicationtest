7be409ee3992a86009f9e7fde7b7a3b66d4b9f02
root <root@example.com>
2024-10-31 16:03:56 +0800

    Save

--- test_vco.py
+++ test_vco.py
@@ -68,11 +68,10 @@ class test_vco(gr.top_block, Qt.QWidget):
         ##################################################
         self.samp_rate = samp_rate = 48000
         self.fc = fc = 2500
-        self.deviation = deviation = 400
-        self.baud_rate = baud_rate = 8
+        self.deviation = deviation = 600
         self.variable_low_pass_filter_taps_0 = variable_low_pass_filter_taps_0 = firdes.low_pass(1.0, samp_rate, fc,100, window.WIN_HAMMING, 6.76)
+        self.sps = sps = int(samp_rate/32)
         self.sensitivity = sensitivity = 2*math.pi*deviation
-        self.interpolation = interpolation = int(samp_rate/baud_rate)
 
         ##################################################
         # Blocks
@@ -117,28 +116,28 @@ class test_vco(gr.top_block, Qt.QWidget):
         self._qtgui_waterfall_sink_x_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_1.qwidget(), Qt.QWidget)
 
         self.top_layout.addWidget(self._qtgui_waterfall_sink_x_1_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            (int(1.5*samp_rate)), #size
+        self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
+            (int(1.5*samp_rate/sps)), #size
             samp_rate, #samp_rate
             "", #name
-            2, #number of inputs
+            1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-3, 3)
+        self.qtgui_time_sink_x_1.set_update_time(0.10)
+        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_1.enable_tags(True)
+        self.qtgui_time_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1.enable_autoscale(False)
+        self.qtgui_time_sink_x_1.enable_grid(False)
+        self.qtgui_time_sink_x_1.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_stem_plot(False)
 
 
-        labels = ['Tx', 'Rx', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
@@ -152,20 +151,32 @@ class test_vco(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(2):
+        for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_1.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
         self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_fcc(1, variable_low_pass_filter_taps_0, fc, samp_rate)
+        self.digital_symbol_sync_xx_0 = digital.symbol_sync_ff(
+            digital.TED_EARLY_LATE,
+            sps,
+            0.045,
+            1.0,
+            1.0,
+            1.5,
+            1,
+            digital.constellation_bpsk().base(),
+            digital.IR_MMSE_8TAP,
+            128,
+            [])
         self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
         self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
             'vco.wav',
@@ -176,8 +187,8 @@ class test_vco(gr.top_block, Qt.QWidget):
             False
             )
         self.blocks_vco_f_0 = blocks.vco_f(samp_rate, sensitivity, 1)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, interpolation)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, sps)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_ff(2)
         self.blocks_char_to_float_1 = blocks.char_to_float(1, 1)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
@@ -185,25 +196,27 @@ class test_vco(gr.top_block, Qt.QWidget):
         self.blocks_add_const_vxx_0 = blocks.add_const_ff((fc/(sensitivity/(2*math.pi))))
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000))), True)
         self.analog_quadrature_demod_cf_1 = analog.quadrature_demod_cf((samp_rate/(2*math.pi*deviation)))
+        self.analog_agc_xx_0 = analog.agc_ff((1e-4), 1.0, 1.0, 2)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_quadrature_demod_cf_1, 0), (self.digital_binary_slicer_fb_0, 0))
+        self.connect((self.analog_agc_xx_0, 0), (self.digital_symbol_sync_xx_0, 0))
+        self.connect((self.analog_quadrature_demod_cf_1, 0), (self.analog_agc_xx_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_vco_f_0, 0))
         self.connect((self.blocks_add_const_vxx_1, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_multiply_const_vxx_1, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_char_to_float_1, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.blocks_char_to_float_1, 0), (self.qtgui_time_sink_x_1, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.blocks_add_const_vxx_1, 0))
-        self.connect((self.blocks_repeat_0, 0), (self.blocks_throttle2_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.blocks_char_to_float_0, 0))
-        self.connect((self.blocks_vco_f_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.blocks_vco_f_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_vco_f_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.blocks_vco_f_0, 0), (self.qtgui_waterfall_sink_x_1, 0))
         self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_char_to_float_1, 0))
+        self.connect((self.digital_symbol_sync_xx_0, 0), (self.digital_binary_slicer_fb_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_quadrature_demod_cf_1, 0))
 
 
@@ -220,11 +233,11 @@ class test_vco(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_interpolation(int(self.samp_rate/self.baud_rate))
+        self.set_sps(int(self.samp_rate/32))
         self.set_variable_low_pass_filter_taps_0(firdes.low_pass(1.0, self.samp_rate, self.fc, 100, window.WIN_HAMMING, 6.76))
         self.analog_quadrature_demod_cf_1.set_gain((self.samp_rate/(2*math.pi*self.deviation)))
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
         self.qtgui_waterfall_sink_x_1.set_frequency_range(0, self.samp_rate)
 
     def get_fc(self):
@@ -244,13 +257,6 @@ class test_vco(gr.top_block, Qt.QWidget):
         self.set_sensitivity(2*math.pi*self.deviation)
         self.analog_quadrature_demod_cf_1.set_gain((self.samp_rate/(2*math.pi*self.deviation)))
 
-    def get_baud_rate(self):
-        return self.baud_rate
-
-    def set_baud_rate(self, baud_rate):
-        self.baud_rate = baud_rate
-        self.set_interpolation(int(self.samp_rate/self.baud_rate))
-
     def get_variable_low_pass_filter_taps_0(self):
         return self.variable_low_pass_filter_taps_0
 
@@ -258,19 +264,20 @@ class test_vco(gr.top_block, Qt.QWidget):
         self.variable_low_pass_filter_taps_0 = variable_low_pass_filter_taps_0
         self.freq_xlating_fir_filter_xxx_0.set_taps(self.variable_low_pass_filter_taps_0)
 
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.blocks_repeat_0.set_interpolation(self.sps)
+        self.digital_symbol_sync_xx_0.set_sps(self.sps)
+
     def get_sensitivity(self):
         return self.sensitivity
 
     def set_sensitivity(self, sensitivity):
         self.sensitivity = sensitivity
         self.blocks_add_const_vxx_0.set_k((self.fc/(self.sensitivity/(2*math.pi))))
-
-    def get_interpolation(self):
-        return self.interpolation
-
-    def set_interpolation(self, interpolation):
-        self.interpolation = interpolation
-        self.blocks_repeat_0.set_interpolation(self.interpolation)
 
 
 
